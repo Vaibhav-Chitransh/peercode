712ccb2ec4febe76c2fc6c74bb8337bda5606a57
Vaibhav Chitransh <vaibhavchitranshbdn@gmail.com>
2025-06-05 19:43:51 +0530

    removed unused buttons from layout in app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import {
   ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedOut,
 
 } from "@clerk/nextjs";
 import { Inter, Space_Grotesk } from "next/font/google";
