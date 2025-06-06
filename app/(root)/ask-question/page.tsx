abdd10320f39203a0a6e6939ae0d846421c72689
Vaibhav Chitransh <vaibhavchitranshbdn@gmail.com>
2025-06-06 13:00:23 +0530

    resolved minor bug in ask a question page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,19 +1,24 @@
 import AskQuestionClient from "./AskQuestionClient";
 import { getUserById } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 // import { redirect } from "next/navigation";
 import React from "react";
 
 const AskQuestion = async () => {
-  // const { userId } = await auth();
-  const userId = "clerk_abc123";
+  const { userId } = await auth();
+  // const userId = "clerk_abc123";
 
-  // if (!userId) {
-  //   console.log("User not authenticated");
-  //   redirect("/sign-in");
-  // }
+  if (!userId) {
+    console.log("User not authenticated");
+    redirect("/sign-in");
+  }
 
   const mongoUser = await getUserById({ userId });
-  console.log(mongoUser);
+  if (!mongoUser) {
+    console.log("User not found in database");
+    redirect("/sign-up"); // or show an error page if you prefer
+  }
 
   return <AskQuestionClient mongoUserId={JSON.stringify(mongoUser._id)} />;
 };
