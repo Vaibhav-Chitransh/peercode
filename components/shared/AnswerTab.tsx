df5ef1fe1b8f2016f0ec46f23c65901ed04f4b18
Naveen-Chhipa <naveenchhipa2003@gmail.com>
2025-06-07 18:52:29 +0530

    PeerCode -7 / 6 /2025 feat added get user info

---
+++ components/shared/AnswerTab.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { getUserAnswers } from "@/lib/actions/user.action";
+import { SearchParamsProps } from "@/types"
+import AnswerCard from "../cards/AnswerCard";
+
+interface AnswerTabProps extends SearchParamsProps {
+    userId: string;
+    clerkId?: string | null;
+}
+
+const AnswerTab = async ({searchParams, userId, clerkId}: AnswerTabProps) => {
+    const result = await getUserAnswers({userId, page: 1});
+
+  return (
+    <>
+      {result.answers.map((item) => (
+        <AnswerCard key={item._id} clerkId={clerkId} _id={item._id} question={item.question} author={item.author} upvotes={item.upvotes.length} createdAt={item.createdAt} />
+      ))}
+    </>
+  )
+}
+
+export default AnswerTab
